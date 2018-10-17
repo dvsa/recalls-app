c913d847e69d024014914d0e98e32412ef2825ff
Magdalena Zakrzewska <zakrzewskam@>
2018-10-17 13:52:43 +0200

    BL-8798 selenium suite skeleton

---
+++ selenium/src/main/java/uk/gov/dvsa/recalls/ui/base/PageIdentityVerificationException.java
@@ -0,0 +1,9 @@
+package uk.gov.dvsa.recalls.ui.base;
+
+public class PageIdentityVerificationException extends RuntimeException {
+
+    public PageIdentityVerificationException(String message) {
+
+        super(message);
+    }
+}
