a20d9a719e87fd14b90a2021b1f1694a46819011
Adrian Zabrocki <A.Zabrocki@kainos.com>
2018-10-30 13:46:36 +0100

    Frontend - map recalls data

--- frontend/service/recallSearch.js
+++ frontend/service/recallSearch.js
@@ -1,5 +1,5 @@
 const request = require('request');
-const RecallDto = require('../../common/dto/recall');
+const RecallDto = require('cvr-common/dto/recall');
 const envVariables = require('../config/environmentVariables');
 
 const RECALLS_BACKEND_URL = envVariables.recallsBackendUrl;
@@ -18,13 +18,38 @@ class RecallSearch {
 
       // TODO: map the data from request body
       console.info(`requested body: ${body}`); // TODO: remove
-      const recalls = [
-        new RecallDto('Audi', 'A6', 'recallNumber', 'defectDesc', 'launchDate', 'concern', 'remedy', '999'),
-        new RecallDto('Make2', 'Model2', 'recallNumber2', 'defectDesc2', 'launchDate2', 'concern2', 'remedy2', '9999'),
+      let recalls = [
+        new RecallDto('Audi', 'A6', 'recallNumber', 'defectDesc', 'launchDate', 'concern', 'remedy', '999'), // TODO: remove
+        new RecallDto('Make2', 'Model2', 'recallNumber2', 'defectDesc2', 'launchDate2', 'concern2', 'remedy2', '9999'), // TODO: remove
       ];
+
+      // TODO: remove adding dummy data to real recalls
+      recalls = recalls.concat(this.mapRecallsToDto(body));
 
       callback(null, recalls);
     });
+  }
+
+  static mapRecallsToDto(body) {
+    const parsedRecalls = JSON.parse(body);
+    const recallsDtos = [];
+
+    for (let i = 0; i < parsedRecalls.length; i += 1) {
+      const recall = parsedRecalls[i];
+      const recallDto = new RecallDto();
+
+      recallDto.make = recall.make;
+      recallDto.model = recall.model;
+      recallDto.recallNumber = recall.recallNumber;
+      recallDto.defectDescription = recall.defectDescription;
+      recallDto.launchDate = recall.launchDate;
+      recallDto.concern = recall.concern;
+      recallDto.remedy = recall.remedy;
+      recallDto.affectedVehiclesNumber = recall.affectedVehiclesNumber;
+
+      recallsDtos.push(recallDto);
+    }
+    return recallsDtos;
   }
 }
 
