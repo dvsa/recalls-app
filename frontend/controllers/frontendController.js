f23a80549f5bf14f6e6af7a0dfaf95ea2d58d3f0
Magdalena Zakrzewska <zakrzewskam@>
2018-11-06 08:57:42 +0100

    BL-8752 plural form of sentence

--- frontend/controllers/frontendController.js
+++ frontend/controllers/frontendController.js
@@ -1,7 +1,9 @@
 const recallSearch = require('../service/recallSearch');
+const pluralForm = require('../service/pluralForm');
 const envVariables = require('../config/environmentVariables');
 
 const ASSETS_BASE_URL = envVariables.assetsBaseUrl;
+const FOUND_RECALLS_COUNT_HEADER = 'This vehicle has <strong>[num] recall{s}.</strong>';
 
 class FrontendController {
   static homePage(response) {
@@ -25,13 +27,14 @@ class FrontendController {
     recallSearch.byMake(make, (err, recalls) => {
       const thisMake = recalls[0].make; // TODO: check errors/nulls
       const model = recalls[0].model;
-      const recallsNumber = recalls.length;
+      const foundRecallsCountHeader = pluralForm
+        .getSingularOrPlural(FOUND_RECALLS_COUNT_HEADER, recalls.length);
 
       response.render('results-page.njk', {
         assetsBaseUrl: ASSETS_BASE_URL,
         make: thisMake,
         model,
-        recallsNumber,
+        foundRecallsCountHeader,
         recalls, // TODO: BL-8752 pass this response to a new page
       });
     });
