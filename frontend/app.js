b7fb19a089b9d676bb5457e0b7e34610568d70f2
Michal Piatek <m.piatel@kainos.com>
2018-11-07 11:58:37 +0100

    Added security headers using helmet
    How it works
    ------------
    
    Helmet is a collection of 14 smaller middleware functions that set HTTP response headers. Running `app.use(helmet())` will not include all of these middleware functions by default.
    
    | Module | Default? |
    |---|---|
    | [contentSecurityPolicy](https://helmetjs.github.io/docs/csp/) for setting Content Security Policy |  |
    | [crossdomain](https://helmetjs.github.io/docs/crossdomain/) for handling Adobe products' crossdomain requests |  |
    | [dnsPrefetchControl](https://helmetjs.github.io/docs/dns-prefetch-control) controls browser DNS prefetching | ✓ |
    | [expectCt](https://helmetjs.github.io/docs/expect-ct/) for handling Certificate Transparency |  |
    | [featurePolicy](https://helmetjs.github.io/docs/feature-policy/) to limit your site's features |  |
    | [frameguard](https://helmetjs.github.io/docs/frameguard/) to prevent clickjacking | ✓ |
    | [hidePoweredBy](https://helmetjs.github.io/docs/hide-powered-by) to remove the X-Powered-By header | ✓ |
    | [hpkp](https://helmetjs.github.io/docs/hpkp/) for HTTP Public Key Pinning |  |
    | [hsts](https://helmetjs.github.io/docs/hsts/) for HTTP Strict Transport Security | ✓ |
    | [ieNoOpen](https://helmetjs.github.io/docs/ienoopen) sets X-Download-Options for IE8+ | ✓ |
    | [noCache](https://helmetjs.github.io/docs/nocache/) to disable client-side caching |  |
    | [noSniff](https://helmetjs.github.io/docs/dont-sniff-mimetype) to keep clients from sniffing the MIME type | ✓ |
    | [referrerPolicy](https://helmetjs.github.io/docs/referrer-policy) to hide the Referer header |  |
    | [xssFilter](https://helmetjs.github.io/docs/xss-filter) adds some small XSS protections | ✓ |
    
    Additionally configured hsts to (instead of the default):
    
    ```javascript
    {
      maxAge: 15768000,
      preload: true,
      includeSubDomains: true
    }
    ```

--- frontend/app.js
+++ frontend/app.js
@@ -2,9 +2,20 @@ const TEMPLATES_PATH = './views';
 
 const express = require('express');
 const nunjucks = require('nunjucks');
+const helmet = require('helmet');
+const hsts = require('hsts');
 const indexRouter = require('./routes');
 
 const app = express();
+
+const HSTS_MAX_AGE = 15768000;
+
+app.use(helmet());
+app.use(hsts({
+  maxAge: HSTS_MAX_AGE,
+  preload: true,
+  includeSubDomains: true,
+}));
 
 nunjucks.configure(TEMPLATES_PATH, {
   autoescape: true,
