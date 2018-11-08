15191a319089cd3e4a1be5989817aa180e65f894
mrogowska <m.rogowska@kainos.com>
2018-11-08 10:48:22 +0100

    BL-8752: create script loading data to the db

--- database/scripts/loadData.js
+++ database/scripts/loadData.js
@@ -1,5 +1,6 @@
 const AWS = require("aws-sdk");
 const CSV = require('fast-csv');
+const Recall = require('../model/recall');
 const ENVIRONMENT = process.env.ENVIRONMENT;
 const RECALLS_TABLE_NAME = `cvr-${ENVIRONMENT}-recalls`;
 const MAKES_TABLE_NAME = `cvr-${ENVIRONMENT}-makes`;
@@ -13,14 +14,8 @@ const vehicleMakes = [];
 const equipmentMakes = [];
 const models = new Map();
 
-function escapeStringOrSetSpaceIfEmpty(string) {
-  return (string === '' || !string) ? ' ' : string.trim();
-}
-
-function mapRecallNumberToRecallType(recallNumber) {
-  const type = recallNumber.split('/')[0];
-  const vehicleTypes = ['R', 'RM', 'RCT', 'RPT', 'RSPV', 'RPC'];
-  return vehicleTypes.includes(type) ? 'vehicle' : 'equipment';
+function trimIfNotEmpty(field) {
+  return (!field || field.length === 0) ? field : field.trim();
 }
 
 function addToArrayIfNotYetExists(array, element) {
@@ -37,71 +32,57 @@ function addToMapIfNotYetExists(map, key, value) {
   }
 }
 
-function Recall(launchDate, recallNumber, make, concern, defect, remedy, vehicleNumber, model, vinStart, vinEnd, buildStart, buildEnd) {
-  this.recallType = mapRecallNumberToRecallType(recallNumber);
-  this.launchDate = escapeStringOrSetSpaceIfEmpty(launchDate);
-  this.recallNumber = escapeStringOrSetSpaceIfEmpty(recallNumber);
-  this.make = escapeStringOrSetSpaceIfEmpty(make);
-  this.concern = escapeStringOrSetSpaceIfEmpty(concern);
-  this.defect = escapeStringOrSetSpaceIfEmpty(defect);
-  this.remedy = escapeStringOrSetSpaceIfEmpty(remedy);
-  this.vehicleNumber = escapeStringOrSetSpaceIfEmpty(vehicleNumber);
-  this.model = escapeStringOrSetSpaceIfEmpty(model);
-  this.vinStart = escapeStringOrSetSpaceIfEmpty(vinStart);
-  this.vinEnd = escapeStringOrSetSpaceIfEmpty(vinEnd);
-  this.buildStart = escapeStringOrSetSpaceIfEmpty(buildStart);
-  this.buildEnd = escapeStringOrSetSpaceIfEmpty(buildEnd);
-
-  if (this.recallType == 'vehicle') {
-    addToArrayIfNotYetExists(vehicleMakes, this.make);
-    addToMapIfNotYetExists(models, 'vehicle-' + this.make, this.model);
+function addToMakesAndModels(recall) {
+  if (recall.type == 'vehicle') {
+    addToArrayIfNotYetExists(vehicleMakes, recall.make);
+    addToMapIfNotYetExists(models, 'vehicle-' + recall.make, recall.model);
   } else {
-    addToArrayIfNotYetExists(equipmentMakes, make);
-    addToMapIfNotYetExists(models, 'equipment-' + this.make, this.model);
+    addToArrayIfNotYetExists(equipmentMakes, recall.make);
+    addToMapIfNotYetExists(models, 'equipment-' + recall.make, recall.model);
   }
 }
 
-// Read csv file and save it as an array of Recalls
-CSV.fromPath('../documents/RecallsFileSmall.csv')
-.on('data', function(line) {
-  if (line[0] != 'Launch Date') {
-    recallData.push(new Recall(line[0], line[1], line[2], line[4], line[5], line[6], line[7], line[9], line[10], line[11], line[12], line[13]));
-  }
-})
-.on('end', function() {
-  // Insert recalls from the array to the db
-  recallData.forEach(function(recall) {
+async function insertRecallsToDb() {
+  for (const recall of recallData) {
     const recallsParams = {
       TableName: RECALLS_TABLE_NAME,
-      Item: {
-        'make_model_recall_number': recall.make + '-' + recall.model + '-' + recall.recallNumber,
-        'type': recall.recallType,
-        'launch_date': recall.launchDate,
-        'recall_number': recall.recallNumber,
-        'make': recall.make,
-        'concern': recall.concern,
-        'defect': recall.defect,
-        'remedy': recall.remedy,
-        'vehicle_number': recall.vehicleNumber,
-        'model': recall.model,
-        'vin_start': recall.vinStart,
-        'vin_end': recall.vinEnd,
-        'build_start': recall.buildStart,
-        'build_end': recall.buildEnd
-      },
-      ReturnConsumedCapacity: 'TOTAL'
+      Item: recall
     };
-    documentClient.put(recallsParams, function(err, data) {
+
+    await documentClient.put(recallsParams, function(err, data) {
       if (err) {
-        console.error('Unable to add recall ', recall.recallType, '. Error JSON:', JSON.stringify(err, null, 2));
+        console.error('Unable to add recall ', recall.make_model_recall_number, '. Error JSON:', JSON.stringify(err, null, 2));
         return process.exit(1);
       } else {
-        console.info(data);
+        console.log('Recall ', recall.make_model_recall_number, ' uploaded successfully to the recalls table');
       }
     });
-  });
+  }
+}
 
-  // Insert makes to the db
+async function insertModelsToDb() {
+  for (const model of models) {
+    const modelsParams = {
+      TableName: MODELS_TABLE_NAME,
+      Item: {
+        'type_make': model[0],
+        'models': model[1].sort(),
+      }
+    };
+
+    await documentClient.put(modelsParams, (err, data) => {
+      if (err) {
+        console.error('Unable to add model ', model[0], '. Error JSON:', JSON.stringify(err, null, 2));
+        return process.exit(1);
+      } else {
+        console.log('Models uploaded successfully to the recalls table');
+      }
+    });
+    console.log("uwaga: " + model);
+  }
+}
+
+function insertMakesToDb() {
   const vehicleMakesParams = {
     TableName: MAKES_TABLE_NAME,
     Item: {
@@ -116,33 +97,39 @@ CSV.fromPath('../documents/RecallsFileSmall.csv')
       'makes': equipmentMakes.sort()
     }
   };
+
   documentClient.put(vehicleMakesParams, (err, data) => {
     if (err) {
-      console.error('Unable to add equipment make. Error JSON:', JSON.stringify(err, null, 2));
+      console.error('Unable to add equipment makes. Error JSON:', JSON.stringify(err, null, 2));
       return process.exit(1);
+    } else {
+      console.log('Equipment makes uploaded successfully to the recalls table');
+      documentClient.put(equipmentMakesParams, (err, data) => {
+        if (err) {
+          console.error('Unable to add vehicle makes. Error JSON:', JSON.stringify(err, null, 2));
+          return process.exit(1);
+        } else {
+          console.log('Vehicle makes uploaded successfully to the recalls table');
+        }
+      });
     }
   });
-  documentClient.put(equipmentMakesParams, (err, data) => {
-    if (err) {
-      console.error('Unable to add vehicle make. Error JSON:', JSON.stringify(err, null, 2));
-      return process.exit(1);
-    }
-  });
+}
 
-  // Insert models to the db
-  models.forEach((value, key, map) => {
-    const modelsParams = {
-      TableName: MODELS_TABLE_NAME,
-      Item: {
-        'type_make': key,
-        'models': value.sort(),
-      }
-    };
-    documentClient.put(modelsParams, (err, data) => {
-      if (err) {
-        console.error('Unable to add model ', key, '. Error JSON:', JSON.stringify(err, null, 2));
-        return process.exit(1);
-      }
-    });
-  });
+// Read csv file
+CSV.fromPath('../documents/RecallsFileSmall.csv')
+.on('data', function(line) {
+  if (line[0] !== 'Launch Date') {
+    const recall = new Recall(trimIfNotEmpty(line[0]), trimIfNotEmpty(line[1]), trimIfNotEmpty(line[2]), trimIfNotEmpty(line[4]), trimIfNotEmpty(line[5]), trimIfNotEmpty(line[6]), trimIfNotEmpty(line[7]), trimIfNotEmpty(line[9]), trimIfNotEmpty(line[10]), trimIfNotEmpty(line[11]), trimIfNotEmpty(line[12]), trimIfNotEmpty(line[13]));
+    recallData.push(recall);
+    addToMakesAndModels(recall);
+  }
+})
+.on('error', function(error) {
+  console.error(error);
+})
+.on('end', function() {
+  insertMakesToDb();
+  insertRecallsToDb();
+  insertModelsToDb();
 });
